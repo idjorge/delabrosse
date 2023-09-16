e9b0dc83085e7d7cbcf30e491bb693e1b03c8ee5
idjorge <60823035+idjorge@users.noreply.github.com>
2023-09-16 21:28:41 +0100

    Add files via upload

---
+++ js/formvalidation.js
@@ -0,0 +1,71 @@
+  // Function onClick button review
+  const formvalidation = () => {
+
+  
+
+    Swal.fire({
+        title: 'Gracias por entregar tu informe',
+        // text:
+        //html: `GRUPO  #${EQUIPO_FILTRADO_2.numero}`,
+        //icon: 'info',
+        // confirmButtonText:
+        footer: 'un cordial saludo',
+        // width:
+        // padding:
+        // background:
+        //grow:
+        // backdrop:
+        timer: 10000,
+        //timerProgressBar: true,
+        // toast:
+        // position:
+        // allowOutsideClick:
+        // allowEscapeKey:
+        // allowEnterKey:
+        // stopKeydownPropagation:
+    
+        // input:
+        // inputPlaceholder:
+        // inputValue:
+        // inputOptions:
+
+            // showConfirmButton:
+        confirmButtonColor: '#00AAE4',
+        // confirmButtonAriaLabel:
+    
+        // showCancelButton:
+        // cancelButtonText:
+        // cancelButtonColor:
+        // cancelButtonAriaLabel:
+        
+        // buttonsStyling:
+        // showCloseButton:
+        // closeButtonAriaLabel:
+    
+    
+        imageUrl: 'media/reportone.png',
+        imageWidth: '250px',
+        // imageHeight:
+        imageAlt: 'reporte',
+    
+        customClass:{
+        // 	container:
+        // 	popup:
+        // 	header:
+        title: 'title-class',
+        // 	closeButton:
+        htmlContainer: 'group-class',
+        // 	icon:
+        // 	image:
+        // 	content:
+        // 	input:
+        // 	actions:
+        // 	confirmButton:
+        // 	cancelButton:
+        // 	footer:	
+        }
+    
+    
+    });
+
+};
